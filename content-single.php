77b03f11403623fc9359658baa4b04be5592c5a6
emilyelaine <ecarey@mcad.edu>
2015-02-15 16:53:44 -0600

    Initial Commit

---
+++ content-single.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package squeezedpressed
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+		<div class="entry-meta">
+			<?php squeezedpressed_posted_on(); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'squeezedpressed' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php squeezedpressed_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
